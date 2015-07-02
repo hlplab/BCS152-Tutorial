5c8318d7b2ea5063767d0d314010437f159a058a
Andrew Watts <awatts@bcs.rochester.edu>
2015-07-02 14:57:06 -0400

    Switch to using Bootstrap alerts on the quiz

--- quiz-1.js
+++ quiz-1.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
     _.templateSettings = {
       interpolate: /\{\{(.+?)\}\}/g
     };
-
-    var template = _.template('<p><strong>Question {{number}}:</strong> The correct answer is <strong>{{answer}}</strong>.</p>');
+    var template = _.template('<div class="alert alert-warning" role="alert">' +
+    '<strong>Question {{number}}:</strong> The correct answer is <strong>{{answer}}</strong>.</div>');
 
   $('#results').on('click', function() {
 
     $('#categorylist').empty();
+    $('#alerts').empty();
 
     var questions = [
       { number: 1, answer: 'a'},
@@ -30,7 +31,7 @@ $(document).ready(function() {
     };
 
     if (! all_answered()) {
-      alert("You're not done yet!");
+      $('#alerts').append('<div class="alert alert-danger" role="alert">You\'re not done yet!</div>');
     } else {
       var incorrect = _.reject(questions, function(e) {
         return $('input[name="q' + e.number + '"]:checked').val() === e.answer;
@@ -38,16 +39,16 @@ $(document).ready(function() {
 
       var catStr = '';
       if (incorrect.length === 0) {
-        catStr = 'You got everything correct! Congrats';
+        $('#alerts').append('<div class="alert alert-success" role="alert">You got everything correct! Congrats</div>')
+        //catStr = 'You got everything correct! Congrats';
       } else {
-        catStr = 'You answered the following questions incorrectly: ' +
-                   _.pluck(incorrect, 'number').join(', ') + '';
+        $('#alerts').append('<div class="alert alert-danger" role="alert">' +
+                            'You answered the following questions incorrectly: ' +
+                            _.pluck(incorrect, 'number').join(', ') + '</div>');
       }
-      $('#categorylist').append('<p>' + catStr + '</p>');
-      $('#categorylist').show('slow');
 
       _.each(incorrect, function(e) {
-        $('#categorylist').append(template(e));
+        $('#alerts').append(template(e));
       });
     }
   });
