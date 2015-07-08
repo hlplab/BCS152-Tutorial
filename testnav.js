0b86bc14f2b09c464ea68b229702075f8a8018ba
Olga <onikolay@u.rochester.edu>
2015-07-08 11:18:51 -0400

    Added new pages, updated the navigation bar to these created pages, updated the continue button on the bottom to progress forward through the tutorial

--- testnav.js
+++ testnav.js
@@ -5,10 +5,10 @@ $('#nav').html(
   '<ul class="nav nav-tabs" role="tablist" style="padding: 45px 200px 25px 300px;">' +
     '<li id="home" role="presentation"><a href="TestBCSFrontpage.html" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>' +
     '<li id="design" role="presentation"><a href="TestBCSDesign.html" aria-controls="design" role="tab" data-toggle="tab">Design</a></li>' +
-    '<li id="method" role="presentation"><a href="#method" aria-controls="method" role="tab" data-toggle="tab">Method</a></li>' +
-    '<li id="createstim" role="presentation"><a href="#create" aria-controls="create" role="tab" data-toggle="tab">Creating Stimuli</a></li>' +
-    '<li id="spr" role="presentation"><a href="#spr" aria-controls="spr" role="tab" data-toggle="tab">Self-Paced Reading</a></li>' +
-    '<li id="analysis" role="presentation"><a href="#analysis" aria-controls="analysis" role="tab" data-toggle="tab">Stimuli Analysis</a></li>' +
+    '<li id="method" role="presentation"><a href="BCSmethod.html" aria-controls="method" role="tab" data-toggle="tab">Method</a></li>' +
+    '<li id="createstim" role="presentation"><a href="BCSStimuli.html" aria-controls="create" role="tab" data-toggle="tab">Creating Stimuli</a></li>' +
+    '<li id="spr" role="presentation"><a href="BCSSPR.html" aria-controls="spr" role="tab" data-toggle="tab">Self-Paced Reading</a></li>' +
+    '<li id="analysis" role="presentation"><a href="BCSAnalysis.html" aria-controls="analysis" role="tab" data-toggle="tab">Stimuli Analysis</a></li>' +
   '</ul>' +
 '</div>');
 //reference to the parent frame to get to the daughter frames.
